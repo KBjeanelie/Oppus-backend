fb9ab0a634f33bd396d342f8f21610a2dece6eae
Jean-Elie <jeaneliekubembula@gmail.com>
2023-01-13 11:10:00 +0100

    MAJ

--- account/views.py
+++ account/views.py
@@ -26,6 +26,7 @@ class UserLoginView(APIView):
     email = serializer.data.get('email')
     password = serializer.data.get('password')
     user = authenticate(email=email, password=password)
+    
     if user is not None:
       token = get_tokens_for_user(user)
       return Response({'token':token, 'msg':'Login Success'}, status=status.HTTP_200_OK)
