3eccd7b6a9018f9c3c932231df215268fc2941c7
Jean-Elie <jeaneliekubembula@gmail.com>
2023-07-05 00:22:29 +0200

    maj

--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='Oppuss-backend API')
+schema_view = get_swagger_view(title='Oppuss-Backend API')
 
 urlpatterns = [
     re_path(r'^$', schema_view),
